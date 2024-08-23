635f211a061f3fcabd00660e118d30a3263c3217
mariosuarezDEV <zs21015977@estudiantes.uv.mx>
2024-08-23 10:44:17 -0600

    Datos listos para implementar diseño de cheques

--- Backend/cheques/views.py
+++ Backend/cheques/views.py
@@ -2,6 +2,8 @@ import datetime
 
 from django.shortcuts import render
 
+from .models import cheque_venta, detalle_cheque
+
 # Create your views here.
 
 def chequesIndexView(request):
@@ -17,14 +19,18 @@ def chequesIndexView(request):
             return render(request, "ChequesIndex.html", {"error": "La forma en la que quieres filtrar los cheques es incorrecta"})
 
 def chequesDetallesView(request):
+    datos = detalle_cheque.objects.all()
+    
     if request.method == 'GET':
-        return render(request, "ChequesDetalles.html") # Renderiza el template index.html
+        return render(request, "ChequesDetalles.html",{
+            'cheques_detalles': datos
+            }) # Renderiza el template index.html
     else:
         fecha_inicio = request.POST.get('fecha-inicio')
         fecha_fin = request.POST.get('fecha-fin')
 
         if fecha_inicio == "" or fecha_fin == "": # las fechas no pueden estar vacias
-            return render(request, "ChequesDetalles.html", {"error": "Debes ingresar ambas fechas"})
+            return render(request, "ChequesDetalles.html", {"error": "Debes ingresar ambas fechas", 'cheques_detalles': datos})
         elif fecha_inicio > fecha_fin:
-            return render(request, "ChequesDetalles.html", {"error": "La forma en la que quieres filtrar los cheques es incorrecta"})
+            return render(request, "ChequesDetalles.html", {"error": "La forma en la que quieres filtrar los cheques es incorrecta", 'cheques_detalles': datos})
 
