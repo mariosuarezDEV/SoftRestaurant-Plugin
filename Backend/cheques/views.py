a05884acf0d54c0bfc39b708d670d3a6358aedf0
Luis Mario cervantes Suarez <zs21015977@estudiantes.uv.mx>
2024-08-23 11:13:15 -0600

    Tablas funcionales

--- Backend/cheques/views.py
+++ Backend/cheques/views.py
@@ -7,8 +7,11 @@ from .models import cheque_venta, detalle_cheque
 # Create your views here.
 
 def chequesIndexView(request):
+    cheques = cheque_venta.objects.all()
     if request.method == 'GET':
-        return render(request, "ChequesIndex.html") # Renderiza el template index.html
+        return render(request, "ChequesIndex.html",{
+            'cheques': cheques
+        }) # Renderiza el template index.html
     else:
         fecha_inicio = request.POST.get('fecha-inicio')
         fecha_fin = request.POST.get('fecha-fin')
