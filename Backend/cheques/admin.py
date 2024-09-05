938d8de604b910d1999c649e03ae5daa9ad96ab3
Luis Mario cervantes Suarez <zs21015977@estudiantes.uv.mx>
2024-09-05 12:37:02 -0600

    Susitucion inversa

--- Backend/cheques/admin.py
+++ Backend/cheques/admin.py
@@ -248,9 +248,105 @@ def sustituir_producto_tres(modeladmin, request, queryset):
             cheqdet.save()
     modeladmin.message_user(request, "El mantenimiento de la venta se hizo correctamente")
 
+def sustituir_producto_cuatro(modeladmin, request, queryset):
+    productos = Productosdetalle.objects.get(idproducto="13005")
+
+    for objeto in queryset: # Cambiar los cheques
+        objeto.cierre = objeto.fecha + datetime.timedelta(minutes=5)
+        objeto.mesa = "P/LL"
+        objeto.nopersonas = 1
+        objeto.cambio = 0
+        objeto.descuento = 0
+        objeto.usuariodescuento = ""
+        objeto.idtipodescuento= ""
+        objeto.tarjetadescuento = 0
+        objeto.totalarticulos = 1
+        objeto.subtotal = productos.preciosinimpuestos * objeto.totalarticulos
+        objeto.total = productos.precio * objeto.totalarticulos
+        objeto.totalconpropina = objeto.total + objeto.propina
+        objeto.totalimpuesto1 = (productos.precio * Decimal(0.16)) * objeto.totalarticulos
+        objeto.totalconcargo = objeto.total + objeto.cargo
+        objeto.totalconpropinacargo = objeto.total + objeto.propina + objeto.cargo
+        objeto.descuentoimporte = 0
+        objeto.efectivo = objeto.total
+        objeto.tarjeta = 0
+        objeto.vales = 0
+        objeto.otros = 0
+        objeto.totalsindescuento = objeto.total
+        objeto.totalalimentos = 0
+        objeto.totalbebidas = 0
+        objeto.totalotros = objeto.total
+        objeto.totaldescuentos = 0 #Duda
+        objeto.totaldescuentoalimentos = 0
+        objeto.totaldescuentobebidas = 0
+        objeto.totaldescuentootros = 0
+        objeto.totalcortesias = 0
+        objeto.totalcortesiaalimentos = 0
+        objeto.totalcortesiabebidas = 0
+        objeto.totalcortesiaotros = 0
+        objeto.totalalimentossindescuentos = 0
+        objeto.totalbebidasindescuentos = 0
+
+        objeto.totaldescuentoycortesia=0
+        objeto.totalalimentossindescuentos=0
+        objeto.totalbebidassindescuentos=0
+        objeto.totalotrossindescuentos=0
+        objeto.descuentocriterios=0
+
+        objeto.totalotrosindescuentos = objeto.total
+        objeto.desc_imp_original = 0
+        objeto.save()
+
+        # Ahora actualiza los registros en Cheqdet
+        for objeto in queryset:
+            # Obtén los IDs de Cheqdet relacionados con el folio
+            cheqdet_ids = list(Cheqdet.objects.filter(foliodet=objeto.folio).values_list('id', flat=True))
+
+            if not cheqdet_ids:
+                continue
+
+            # Si hay más de un ID, elimina todos menos el primero
+            if len(cheqdet_ids) > 1:
+                ids_to_delete = cheqdet_ids[1:]  # Excluye el primero
+                Cheqdet.objects.filter(id__in=ids_to_delete).delete()
+
+            # Modifica el primer registro
+            cheqdet = Cheqdet.objects.get(id=cheqdet_ids[0])
+            cheqdet.cantidad = 2
+            try:
+                producto = Productos.objects.get(idproducto="13005")
+            except Productos.DoesNotExist:
+                modeladmin.message_user(request, "El producto con id '13005' no existe.")
+                return
+
+            cheqdet.idproducto = producto
+            cheqdet.descuento = 0
+            cheqdet.precio = productos.precio
+            cheqdet.impuesto1 = (productos.precio * Decimal(0.16))
+            cheqdet.preciosinimpuestos = productos.preciosinimpuestos
+            cheqdet.modificador = False
+            cheqdet.comentario = ""
+            cheqdet.usuariodescuento = ""
+            cheqdet.comentariodescuento = ""
+            cheqdet.comentario = ""
+            cheqdet.idtipodescuento = ""
+            cheqdet.preciocatalogo = productos.precio
+
+            cheqdet.save()
+    modeladmin.message_user(request, "El mantenimiento de la venta se hizo correctamente")
+
+def sustituye_inversa(produto_id, cantidad):
+    pass
+
+def sustituye_por_Botella_don_julio(modeladmin, request, queryset):
+    for cheque in queryset: # Recorrer cheqdet
+        cheqsdet = Cheqdet.objects.get(foliodet=cheque.folio)
+        print(cheqsdet)
+
 sustituir_producto_uno.short_description = "Sustituir por Café en grano 1/4"
 sustituir_producto_dos.short_description = "Sustituir por Pan para llevar"
 sustituir_producto_tres.short_description = "Sustituir por Cargas de café"
+sustituye_por_Botella_don_julio.short_description = "Sustituir por Botella Tequilia Don Julio 70 Aniversario"
 
 class TotalImpuesto1Filter(admin.SimpleListFilter):
     title = 'Impuesto' # Nombre que se mostrara en el filtro
@@ -298,7 +394,7 @@ class ChequesAdmin(admin.ModelAdmin):
     date_hierarchy = "fecha"
     search_fields = ("folio", "mesa", )
     list_display=("folio", "fecha", "mesa", "facturado", "totalarticulos", "subtotal", "total", "efectivo", "tarjeta", "totalimpuesto1" )
-    actions = [sustituir_producto_uno, sustituir_producto_dos, sustituir_producto_tres,]
+    actions = [sustituir_producto_uno, sustituir_producto_dos, sustituir_producto_tres, sustituye_por_Botella_don_julio, ] #admin1233
 
 @admin.register(Cheqdet)
 class CheqdetAdmin(admin.ModelAdmin):
